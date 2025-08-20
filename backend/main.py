7e21cec7969a4c8b7c06541146f39927f08ba2b0
Rovee21 <ro.vashisht21@gmail.com>
2025-08-20 18:56:17 -0400

    fixing errors

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from dot_env import load_dotenv
+from dotenv import load_dotenv
 import requests, os
 from fastapi.responses import RedirectResponse, JSONResponse
+from pathlib import Path
 
 load_dotenv()
 
 app = FastAPI()
 
-CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "https://localhost:8000/callback"
-
