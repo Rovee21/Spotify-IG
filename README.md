b1756d82dbb416bc07a5dbc1b31138c2afe801d8
Rovee21 <ro.vashisht21@gmail.com>
2025-08-27 19:37:40 -0400

    Ngrok for backend hosting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Spotify-IG
+
+To run it right now you have to run the backend with this command: ./venv/bin/uvicorn main:app --reload 
+To run the server in a new terminal, use this command: ngrok http 8000
+To run the frontend, use this command: npm run dev
--- backend/main.py
+++ backend/main.py
@@ -10,4 +10,37 @@ app = FastAPI()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = "https://localhost:8000/callback"
+REDIRECT_URI = "https://a4d1507f0a69.ngrok-free.app"
+
+
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_ME_URL = "https://api.spotify.com/v1/me"
+
+@app.get("/login")
+def login():
+    scope = "user-read-email user-read-private user-read-recently-played"
+    auth_url = (
+        f"{SPOTIFY_AUTH_URL}?response_type=code&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URI}&scope={scope}"
+    )
+    return RedirectResponse(auth_url)
+
+# @app.get("/callback")
+# def callback(code: str):
+#     payload = {
+#         "grant_type": "authorization_code",
+#         "code": code,
+#         "redirect_uri": REDIRECT_URI,
+#         "client_id": CLIENT_ID,
+#         "client_secret": CLIENT_SECRET,
+#     }
+#     response = requests.post(SPOTIFY_TOKEN_URL, data=payload)
+#     token_info = response.json()
+#     access_token = token_info.get("access_token")
+#     headers = {"Authorization": f"Bearer {access_token}"}
+
+#     # Fetch user profile
+#     user_profile = requests.get(SPOTIFY_ME_URL, headers=headers).json()
+
+#     return JSONResponse(content=user_profile)
